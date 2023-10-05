f8f115c60d73c30c60838f8f6e4a2ee1f0329fcb
Shreenidhi <Shreenidhi@ACER1214>
2023-10-05 16:46:38 +0530

    Add project files.

---
+++ MoviePresentationApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using MyMovieStoreAppClassLibrary;
+namespace MoviePresentationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieController controller = new MovieController();
+            controller.Start();
+        }
+    }
+}
